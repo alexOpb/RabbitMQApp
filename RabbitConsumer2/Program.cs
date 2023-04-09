610f4683dde0aafa4a566e71e809f25eda405898
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2023-04-09 20:02:58 +0600

    Initial commit

---
+++ RabbitConsumer2/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new()  
+{  
+    Uri = new Uri("amqp://myuser:mypassword@localhost:5672/myvhost"),  
+    ClientProvidedName = "Rabbit Consumer App 2"  
+};  
+var cnn = factory.CreateConnection();  
+var channel = cnn.CreateModel();
+
+const string queueName = "SampleQueue";
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(1000);
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message consumed: {message}");
+    channel.BasicAck(args.DeliveryTag, multiple: false);
+};
+
+var consumerTag = channel.BasicConsume(queueName, autoAck: false, consumer);
+
+Console.ReadKey();
+channel.BasicCancel(consumerTag);
+channel.Close();
+cnn.Close();
---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;  
+using System.Text;  
+  
+ConnectionFactory factory = new()  
+{  
+    Uri = new Uri("amqp://myuser:mypassword@localhost:5672/myvhost"),  
+    ClientProvidedName = "Rabbit Publisher App"  
+};  
+var cnn = factory.CreateConnection();  
+var channel = cnn.CreateModel();  
+  
+const string exchangeName = "SampleExchange";  
+const string routingKey = "sample-routing-key";  
+const string queueName = "SampleQueue";  
+  
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);  
+channel.QueueDeclare(queueName, false, false, false, null);  
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+for (var i = 0; i < 100; i++)
+{
+    var messageBodyBytes = Encoding.UTF8.GetBytes($"Message {i}");  
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    Console.WriteLine($"Publishing message: Message ${i}");
+    await Task.Delay(1000);
+}
+
+channel.Close();  
+cnn.Close();
